0f957ee12c57473dbe948162eabe7858e0874d4a
arf-afk <sugarbomps@gmail.com>
2024-08-29 09:28:20 +0800

    myfirstUpload

---
+++ resources/views/albitarf.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Albit Description</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+<body class = "bg-sky-200">
+    <h1 class="underline decoration-wavy text-teal-600 hover:text-fuchsia-500 hover:no-underline">My name is Allen Albit gwapo kaayo sa wa pa mauso ang taw.</h1>
+</body>
+</html>
